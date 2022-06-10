0e0ad631ce33218b4de78746b07ba7cb02480592
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2022-06-10 13:51:14 -0500

    removes unused components

--- graphiql-v2/src/components/InlineEditor/EditorActions/styles.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from '@graphiql-v2-prototype/graphiql-ui-library';
-
-export const EditorActionsStyled = styled('div', {
-  display: 'flex',
-  flexDirection: 'column',
-  gap: 16,
-  width: 40,
-  minWidth: 40,
-});
--- graphiql-v2/src/components/InlineEditor/editors/OperationsEditor/index.ts
+++
@@ -1,1 +0,0 @@
-export { OperationsEditor } from './OperationsEditor';
