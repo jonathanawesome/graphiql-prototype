ad6f947620da643f8fd4e7c99632cf198d6d640b
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2022-06-19 12:17:46 -0500

    re-add styles to coordinate monaco width in flex container

--- graphiql-editor/src/components/MonacoEditor/styles.ts
+++ graphiql-editor/src/components/MonacoEditor/styles.ts
@@ -10,6 +10,9 @@ export const MonacoEditorStyled = styled('div', {
 export const MonacoWrap = styled('div', {
   height: '100%',
   width: '100%',
+  overflow: 'hidden',
+  position: 'absolute',
+  minWidth: '300px',
 
   variants: {
     editorType: {
