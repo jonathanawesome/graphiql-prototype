8b655c0aa27b12adc8b575b44d96485d360372f7
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2022-07-08 11:26:08 -0500

    removes api from repository

--- apps/api/src/modules/Base/index.ts
+++
@@ -1,9 +0,0 @@
-import { createModule } from 'graphql-modules';
-
-import { BaseTypeDefs } from './typeDefs';
-
-export const BaseModule = createModule({
-  id: 'Base',
-  dirname: __dirname,
-  typeDefs: [BaseTypeDefs],
-});
--- apps/api/src/types.ts
+++
@@ -1,6 +0,0 @@
-import { YogaInitialContext } from '@graphql-yoga/node';
-import { PrismaClient } from '../src/prisma/generated';
-
-export type Context = YogaInitialContext & {
-  prisma: PrismaClient;
-};
