2b8f2d5e38f73a2881b4f94266ef8f7e08b20ae9
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2022-09-26 22:20:02 -0500

    should be working...

--- packages/core/editor/src/components/MonacoEditor/MonacoEditor.tsx
+++ packages/core/editor/src/components/MonacoEditor/MonacoEditor.tsx
@@ -3,7 +3,6 @@ import { editor as MONACO_EDITOR } from 'monaco-editor';
 
 // hooks
 import { MonacoEditorTypes, useEditor } from '@graphiql-prototype/use-editor';
-import { useSchema } from '@graphiql-prototype/use-schema';
 
 // styles
 import { MonacoEditorStyled, MonacoWrap } from './styles';
@@ -19,24 +18,18 @@ export const MonacoEditor = ({
 }) => {
   const monacoEditorRef = useRef<HTMLDivElement>(null);
 
-  const { schemaLoading } = useSchema();
-  const { monacoEditors } = useEditor();
-
   // console.log('rendering MonacoEditor', {
   //   editorRef,
   // });
 
   useEffect(() => {
-    // if (!schemaLoading && !monacoEditors[monacoEditorType]) {
-    // if (!monacoEditors[monacoEditorType]) {
     initMonacoEditor({
       monacoEditorType,
       monacoEditorRef: monacoEditorRef.current as unknown as HTMLDivElement,
       optionOverrides,
     });
-    // }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [schemaLoading]);
+  }, []);
 
   return (
     <MonacoEditorStyled>
