5d9ff9cbd25251ddc238e4158e5b1e726dc91814
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2022-10-29 16:42:20 -0500

    removes unused radix code

--- packages/core/ui-library/src/components/Tabs/Tabs.tsx
+++ packages/core/ui-library/src/components/Tabs/Tabs.tsx
@@ -1,102 +1,14 @@
 import { useEffect, useState } from 'react';
-// import type { Dispatch, MouseEvent, SetStateAction } from 'react';
 
 // components
-// import { ChevronLarge } from '../../icons';
 import { TabsList } from './TabsList';
 import { TabPanels } from './TabPanels';
 
 // styles
-import {
-  // StyledCollapsibleContent,
-  // StyledCollapsibleRoot,
-  // StyledCollapsibleTrigger,
-  // StyledTabsRoot,
-  StyledTabs,
-} from './styles';
+import { StyledTabs } from './styles';
 
 // types
 import { TabsProps } from './types';
-
-// const NotCollapsible = ({
-//   ariaLabel,
-//   selectedTab,
-//   setSelectedTab,
-//   doRemoveTab,
-//   tabbedContent,
-// }: TabsProps & {
-//   selectedTab: string;
-//   setSelectedTab: Dispatch<SetStateAction<string>>;
-// }) => {
-//   return (
-//     <StyledTabsRoot value={selectedTab} onValueChange={(value) => setSelectedTab(value)}>
-//       <TabsList
-//         ariaLabel={ariaLabel}
-//         doRemoveTab={doRemoveTab}
-//         setSelectedTab={setSelectedTab}
-//         tabbedContent={tabbedContent}
-//       />
-//       <TabPanels selectedTab={selectedTab} tabbedContent={tabbedContent} />
-//     </StyledTabsRoot>
-//   );
-// };
-
-// const Collapsible = ({
-//   ariaLabel,
-//   selectedTab,
-//   setSelectedTab,
-//   doRemoveTab,
-//   tabbedContent,
-// }: TabsProps & {
-//   selectedTab: string;
-//   setSelectedTab: Dispatch<SetStateAction<string>>;
-// }) => {
-//   const [isOpen, setIsOpen] = useState<boolean>(false);
-
-//   const handleCollapseOnClick = ({ event }: { event: MouseEvent }) => {
-//     const targetValue = event.currentTarget.getAttribute('data-value');
-//     // console.log('handleCollapseOnClick', {
-//     //   selectedTab,
-//     //   targetValue,
-//     // });
-//     if (selectedTab === targetValue && isOpen) {
-//       setIsOpen(false);
-//     }
-//     if (selectedTab === targetValue && !isOpen) {
-//       setIsOpen(true);
-//     }
-//     if (selectedTab !== targetValue && !isOpen) {
-//       setIsOpen(true);
-//     }
-//   };
-
-//   return (
-//     <StyledCollapsibleRoot
-//       open={isOpen}
-//       onOpenChange={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
-//     >
-//       <StyledTabsRoot
-//         value={selectedTab}
-//         onValueChange={(value) => setSelectedTab(value)}
-//       >
-//         <TabsList
-//           ariaLabel={ariaLabel}
-//           doRemoveTab={doRemoveTab}
-//           handleCollapseOnClick={handleCollapseOnClick}
-//           isCollapsible={true}
-//           setSelectedTab={setSelectedTab}
-//           tabbedContent={tabbedContent}
-//         />
-//         <StyledCollapsibleTrigger isOpen={isOpen}>
-//           <ChevronLarge />
-//         </StyledCollapsibleTrigger>
-//         <StyledCollapsibleContent forceMount hidden={!isOpen}>
-//           <TabPanels selectedTab={selectedTab} tabbedContent={tabbedContent} />
-//         </StyledCollapsibleContent>
-//       </StyledTabsRoot>
-//     </StyledCollapsibleRoot>
-//   );
-// };
 
 export const Tabs = ({
   initialSelectedTab,
@@ -127,25 +39,4 @@ export const Tabs = ({
       <TabPanels selectedTab={selectedTab} tabbedContent={tabbedContent} />
     </StyledTabs>
   );
-
-  // if (isCollapsible) {
-  //   return (
-  //     <Collapsible
-  //       ariaLabel={ariaLabel}
-  //       selectedTab={selectedTab}
-  //       setSelectedTab={setSelectedTab}
-  //       doRemoveTab={doRemoveTab}
-  //       tabbedContent={tabbedContent}
-  //     />
-  //   );
-  // }
-  // return (
-  //   <NotCollapsible
-  //     ariaLabel={ariaLabel}
-  //     selectedTab={selectedTab}
-  //     setSelectedTab={setSelectedTab}
-  //     doRemoveTab={doRemoveTab}
-  //     tabbedContent={tabbedContent}
-  //   />
-  // );
 };
--- packages/core/ui-library/src/components/Tabs/styles.ts
+++ packages/core/ui-library/src/components/Tabs/styles.ts
@@ -1,61 +1,7 @@
-import { styled, theme } from '../../theme';
-
-// import * as TabsPrimitive from '@radix-ui/react-tabs';
-// import * as Collapsible from '@radix-ui/react-collapsible';
+import { styled } from '../../theme';
 
 export const StyledTabs = styled('section', {
   position: 'relative',
   height: '100%',
   width: '100%',
 });
-
-// export const StyledTabsRoot = styled(TabsPrimitive.Root, {
-//   position: 'relative',
-//   height: '100%',
-//   width: '100%',
-// });
-
-// export const StyledCollapsibleRoot = styled(Collapsible.Root, {
-//   width: '100%',
-//   hairlineT: theme.colors.surface3,
-// });
-
-// export const StyledCollapsibleTrigger = styled(Collapsible.Trigger, {
-//   all: 'reset',
-//   position: 'absolute',
-//   top: 0,
-//   right: 0,
-//   display: 'flex',
-//   alignItems: 'center',
-//   justifyContent: 'center',
-//   cursor: 'pointer',
-//   width: theme.space[10],
-//   height: theme.space[10],
-
-//   svg: {
-//     width: theme.space[4],
-//     height: theme.space[4],
-//     path: {
-//       fill: theme.colors.text4,
-//     },
-//   },
-
-//   '&:hover': {
-//     backgroundColor: theme.colors.surface2,
-//     svg: {
-//       path: {
-//         fill: theme.colors.text1,
-//       },
-//     },
-//   },
-
-//   variants: {
-//     isOpen: {
-//       true: {
-//         transform: 'rotate(180deg)',
-//       },
-//     },
-//   },
-// });
-
-// export const StyledCollapsibleContent = styled(Collapsible.Content, {});
