f05128a363d6ee34bcdd5e83d8d6a158b0b7ed90
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2022-11-18 11:54:35 -0600

    add build to /ui-library

---
+++ packages/core/ui-library/vite.config.ts
@@ -0,0 +1,37 @@
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import pluginReact from '@vitejs/plugin-react';
+
+export default defineConfig(() => {
+  return {
+    plugins: [pluginReact()],
+    build: {
+      lib: {
+        entry: 'src/index.ts',
+        fileName: 'graphiql-prototype-core-ui-library',
+        formats: ['cjs', 'es'],
+      },
+      rollupOptions: {
+        external: [
+          '@graphiql-prototype/store',
+          'monaco-editor',
+          'graphql',
+          'react',
+          'react-dom',
+        ],
+        output: {
+          chunkFileNames: '[name].[format].js',
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+            'monaco-editor': 'MonacoEditor',
+            react: 'React',
+            'react-dom': 'ReactDOM',
+            graphql: 'GraphQL',
+          },
+        },
+      },
+    },
+  };
+});
